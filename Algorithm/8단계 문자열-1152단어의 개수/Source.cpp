cff6bec0629a79afe7539a4b46adc3ed7c1c608b
Chang Su Choi <choics2623@gmail.com>
2020-08-14 22:16:03 +0900

    Not ready :(

---
+++ Algorithm/8단계 문자열-1152단어의 개수/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string a;
+	cin >> a;
+	int cnt = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (a[i] == cin.eof())
+			break;
+		if (a[i] == ' ')
+		{
+			if (a[i + 1] == cin.eof())
+				break;
+			else if (a[i + 1] != ' ')
+				cnt++;
+		}
+		else
+		{
+			if (a[i + 1] == cin.eof()|| a[i + 1] != ' ')
+				break;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
