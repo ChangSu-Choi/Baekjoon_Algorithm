ff70f7499d4a77c5dedce5c029104c4a9c7893ce
Chang Su Choi <choics2623@gmail.com>
2020-08-22 00:21:42 +0900

    문제 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
    단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
    
    출력
    첫째 줄에 그룹 단어의 개수를 출력한다.

--- Algorithm/8단계 문자열-1316그룹 단어 체커/Source.cpp
+++ Algorithm/8단계 문자열-1316그룹 단어 체커/Source.cpp
@@ -4,22 +4,20 @@ using namespace std;
 
 int distin_word(string a)
 {
-	if (!a.empty())
+	if (a.length() == 1)
+		return 1;
+	for (int i = 0; i < a.length() - 2; i++)
 	{
-		int temp;
-		int i = 0;
-		int find_word = a.find(a[i]);		//첫번째 글자를 찾으면 그 인덱스
-		while (find_word != a.npos)			
+		if (a[i] != a[i + 1])
 		{
-			temp = find_word;
-			find_word = a.find(a[i], find_word + 1);		//다음에 같은 글자인지
-			if (find_word == a.npos)			//같은 글자가 없으면 반복문 수행
-				continue;
-			else if ((find_word - temp) > 1)	//같은 글자가 띄어져 있으면 리턴 0
-				return 0;
-			i++;
+			for (int j = i + 2; j < a.length(); j++)
+			{
+				if (a[j] == a[i])
+					return 0;
+			}
 		}
 	}
+
 	return 1;
 }
 
@@ -27,12 +25,12 @@ int main()
 {
 	int n;
 	cin >> n;
-	string* a = new string[n];
+	string a;
 	int result = 0;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> a[i];
-		result += distin_word(a[i]);
+		cin >> a;
+		result += distin_word(a);
 	}
 	
 	cout << result;
